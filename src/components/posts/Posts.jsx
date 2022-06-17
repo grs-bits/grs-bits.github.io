f12498599ab6181c1a2cfdac0e19c74eccd4ae66
Robins <mehrarobins07@gmail.com>
2022-06-17 14:27:26 +0530

    image change
    changes line 11 with an image

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -8,7 +8,7 @@ export default function Posts() {
       <Post img="https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
       <Post img="https://images.pexels.com/photos/219998/pexels-photo-219998.jpeg"/>
       <Post img="https://images.pexels.com/photos/7991579/pexels-photo-7991579.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
-      {/* <Post img="https://images.pexels.com/photos/248547/pexels-photo-248547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/> */}
+      <Post img="https://images.pexels.com/photos/248547/pexels-photo-248547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
     </div>
   );
 }
